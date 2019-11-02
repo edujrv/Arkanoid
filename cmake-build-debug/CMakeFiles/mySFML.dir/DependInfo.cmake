61dea173f874b26eb621578d432197a3cbaa946c
edujrv <55923936+edujrv@users.noreply.github.com>
2019-11-02 16:37:10 -0300

    quizas ahora funca en windows

---
+++ cmake-build-debug/CMakeFiles/mySFML.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/Ball.cpp" "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/cmake-build-debug/CMakeFiles/mySFML.dir/Ball.cpp.obj"
+  "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/Ladrillo.cpp" "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/cmake-build-debug/CMakeFiles/mySFML.dir/Ladrillo.cpp.obj"
+  "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/Menu.cpp" "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/cmake-build-debug/CMakeFiles/mySFML.dir/Menu.cpp.obj"
+  "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/Playerbar.cpp" "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/cmake-build-debug/CMakeFiles/mySFML.dir/Playerbar.cpp.obj"
+  "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/Textos.cpp" "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/cmake-build-debug/CMakeFiles/mySFML.dir/Textos.cpp.obj"
+  "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/Vidas.cpp" "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/cmake-build-debug/CMakeFiles/mySFML.dir/Vidas.cpp.obj"
+  "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/main.cpp" "C:/Users/usuario/Desktop/Arkanoid V 2.0/Arkanoid/cmake-build-debug/CMakeFiles/mySFML.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "D:/Program Files (x86)/JetBrians/CLion 2019.2/mingw32/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
